b3e7c7923f7223637a350bfd731882197245f24d
Гуров Георгий <magnezia98@mail.ru>
2020-07-08 22:06:28 +0300

    Header done

---
+++ source/js/blocks/header/header-components/year-collection.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import '../../../../img/watch_example.png';
+import '../../../../img/Ellipse.svg';
+
+import SocialLinks from '../../utils/social-links';
+
+export default function YearCollection() {
+  const collectionInfo = {
+    collectionName: 'Весна/Лето 2019',
+    type: 'Коллекция',
+    description: 'Швейцарские часы в наличии в Москве и с доставкой по всему миру',
+    modelExamplePrice: '20 000',
+    modelExampleDiameter: '28 мм диаметр',
+  };
+  return (
+    <section className="year-collection">
+      <SocialLinks />
+      <div className="year-collection__container">
+        <h1 className="year-collection__collection-name">{collectionInfo.collectionName}</h1>
+        <p className="year-collection__type">{collectionInfo.type}</p>
+        <p className="year-collection__description">{collectionInfo.description}</p>
+        <button className="year-collection__see-catalog" type="button">Смотреть каталог</button>
+      </div>
+      <div className="example-of-watch">
+        <p className="example-of-watch__info example-of-watch__info--price">{collectionInfo.modelExamplePrice} &#8381;</p>
+        <p className="example-of-watch__info example-of-watch__info--diameter">{collectionInfo.modelExampleDiameter}</p>
+      </div>
+    </section>
+  );
+}
--- source/js/blocks/header/header-site.jsx
+++ source/js/blocks/header/header-site.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import BurgerAndLogo from './header-components/burger-and-logo';
 import UserButtonPanel from './header-components/user-button-panel';
+import YearCollection from './header-components/year-collection';
 
 export default function Header() {
   return (
     <header className="header-site">
-      <BurgerAndLogo />
-      <UserButtonPanel />
+      <section className="top-bar-site">
+        <BurgerAndLogo />
+        <UserButtonPanel />
+      </section>
+      <YearCollection />
     </header>
   );
 }
